fa92f90ea1f3260263aa07b08f9e075135a5ca9d
Mart Groothuis <martgroothuis@gmail.com>
2022-12-05 13:09:10 +0100

    docs: add licence

--- src/userscript.ts
+++ src/userscript.ts
@@ -3,10 +3,10 @@ export default {
   name: "ELO",
   version: "0.0.1",
   description: "ELO usuablity improvements",
-  author: "Mart Groothuis",
+  author: "Joost Godschalk & Mart Groothuis",
   hostname: "leren.windesheim.nl",
   sitename: "leren.windesheim.nl",
-  namespace: "https://github.com/Mart-0",
+  namespace: "https://github.com/MartGroothuis/",
   runAt: "document-end",
   hostedAt: "https://example.com/userscripts/",
 } as const;
