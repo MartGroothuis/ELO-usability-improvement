1e557d64ca8f71fac56b7e640621556a8b0bf927
Joost Godschalk <45271912+TheLazyTurtle@users.noreply.github.com>
2022-12-12 15:05:29 +0100

    Fixed header image size

---
+++ src/operations/global/modifyHeaderImg.ts
@@ -0,0 +1,11 @@
+import { Actions } from '~src/controller/actions'
+import { GlobalRepository } from '~src/repository/globalRepository'
+
+export default () => {
+  GlobalRepository.getInstance().getHeaderImg()
+    .then((headerImg) => {
+      console.log(headerImg)
+      headerImg.style.maxHeight = '45px'
+      Actions.addClassName(headerImg, 'small-header-img')
+    })
+}
--- src/repository/globalRepository.ts
+++ src/repository/globalRepository.ts
@@ -1,64 +1,86 @@
-import { Actions } from "~src/controller/actions";
-
 export class GlobalRepository {
-  private static instance: GlobalRepository;
-  private rootElement: string;
+  private static instance: GlobalRepository
+  private rootElement: string
 
   private constructor(rootElement: string) {
-    this.rootElement = rootElement;
+    this.rootElement = rootElement
   }
 
   public static getInstance() {
     if (this.instance === null || this.instance === undefined) {
-      this.instance = new GlobalRepository("");
+      this.instance = new GlobalRepository('')
     }
 
-    return this.instance;
+    return this.instance
   }
 
   // This is the main menu bar
   public async getMenuBar(): Promise<Element> {
     // try every 10 ms to find this element and return a promise when it is found
-    let tries = 0;
+    let tries = 0
     return new Promise((resolve, reject) => {
-      let interval = setInterval(() => {
-        tries++;
+      const interval = setInterval(() => {
+        tries++
 
-        let menuBar = document.getElementsByClassName(
-          "d2l-branding-navigation-background-color d2l-visible-on-ancestor-target"
-        )[0];
+        const menuBar = document.getElementsByClassName(
+          'd2l-branding-navigation-background-color d2l-visible-on-ancestor-target'
+        )[0]
 
         if (menuBar) {
-          clearInterval(interval);
-          resolve(menuBar);
+          clearInterval(interval)
+          resolve(menuBar)
         }
         if (tries > 1000) {
-          clearInterval(interval);
-          reject();
+          clearInterval(interval)
+          reject()
         }
-      }, 10);
-    });
+      }, 10)
+    })
   }
 
   // get the d2l-navigation-main-header
   public async getNavigation(): Promise<Element> {
-    let tries = 0;
+    let tries = 0
     return new Promise((resolve, reject) => {
-      let interval = setInterval(() => {
-        tries++;
+      const interval = setInterval(() => {
+        tries++
 
         //d2l-navigation-main-header querySelector
-        let navigation = document.querySelector("d2l-navigation-main-header");
+        const navigation = document.querySelector('d2l-navigation-main-header')
 
         if (navigation?.shadowRoot) {
-          clearInterval(interval);
-          resolve(navigation);
+          clearInterval(interval)
+          resolve(navigation)
         }
         if (tries > 1000) {
-          clearInterval(interval);
-          reject();
+          clearInterval(interval)
+          reject()
         }
-      }, 10);
-    });
+      }, 10)
+    })
+  }
+
+  // Get the header img
+  public async getHeaderImg(): Promise<HTMLImageElement> {
+    let tries = 0
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        tries++
+
+        const headerImg = document.querySelector('d2l-navigation-link-image')
+        const shadowRoot = headerImg?.shadowRoot?.children[0];
+        const imgWrapper = shadowRoot?.children[1];
+        const img = imgWrapper?.children[0] as HTMLImageElement;
+
+        if (img) {
+          clearInterval(interval)
+          resolve(img)
+        }
+        if (tries > 1000) {
+          clearInterval(interval)
+          reject()
+        }
+      }, 10)
+    })
   }
 }
